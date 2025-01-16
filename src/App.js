e351e9e921c1ef3288a685a3d9347af46c78424a
Aviya Yechezkel <aviyay@edu.acc.ac.il>
2025-01-16 21:36:07 +0200

    add

---
+++ src/App.js
@@ -0,0 +1,35 @@
+import logo from './logo.svg';
+import './App.css';
+import {BrowserRouter, Link, Route, Routes} from "react-router-dom";
+import Login from "./Login";
+import HomePage from "./HomePage";
+import Register from "./Register";
+import {useState} from "react";
+import Navbar from "./Navbar";
+import Image from "./Image";
+import TodoList from "./TodoList";
+
+function App() {
+    const [toHomePage,setToHomePage] = useState("false");
+
+  return (
+    <div className="App">
+
+      <BrowserRouter>
+          <Navbar/>
+          {/*<Link to={"/register"} onClick={()=>{setToHomePage(true)}}>Register</Link>*/}
+          {/*<Link to={"/login"} onClick={()=>{setToHomePage(true)}}>Login</Link>*/}
+          {/*{toHomePage===true&&<Link to={"/"} onClick={()=>{setToHomePage(false)}}>Homepage</Link>}*/}
+         <Routes>
+             <Route path="/"  element={<HomePage/>} />
+             <Route path={"/login"} element={<Login/>}/>
+             <Route path={"/register"} element={<Register/>}/>
+             <Route path={"/image"} element={ <Image/>}/>
+             <Route path={"/todo"} element={ <TodoList/>}/>
+         </Routes>
+      </BrowserRouter>
+    </div>
+  );
+}
+
+export default App;
